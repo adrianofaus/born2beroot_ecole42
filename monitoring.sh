f5f7962ae7b8e9503d3fabe34b4757d1633ec4f0
Adriano Faustino <63206471+adrianofaus@users.noreply.github.com>
2021-11-16 21:36:05 -0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -10,7 +10,7 @@ lboo=$(who -b | cut -d " " -f12-)
 lvmu=$(lsblk | grep "lvm" | awk '{if($1){printf "yes"; exit;} else {prntf "no";}}')
 ctcp=$(netstat -an | grep "ESTABLISHED" | wc -l | awk '{if ($1 > 0) {printf "%d ESTABILISHED\n", $1; exit;} else {printf "no";}}')
 ulog=$(who | cut -d " " -f 1 | sort -u | wc -l)
-ntip=$(hostname -I | awk '{printf "%s ", $1}' `"("`ip a | grep "link/ether" | awk '{print $2}'`")")
+ntip=$(hostname -I | awk '{printf "%s ", $1}' "(" ip a | grep "link/ether" | awk '{print $2}' ")")
 sudo=$(cat /var/log/sudo/sudo.log | grep -a "COMMAND" | wc -l 'cmd')
 
 wall "\
